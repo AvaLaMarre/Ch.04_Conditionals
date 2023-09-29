7a9fa100ea0a65d83713ac0cf85953d2e7ae830b
lamarreava <lamarreava@urbandaleschools.com>
2023-09-29 11:43:24 -0500

    commit mid

--- 4.0_Jedi_Training.py
+++ 4.0_Jedi_Training.py
@@ -8,44 +8,46 @@ if midichlorians > 10000:
     print("You have serious Jedi potential")
 else:
     print("Jedi, you will never be.")
-#missing a ) first line
+#missing a ")" first line
 #there is no : at the end of the if statement
 #line nine should be else not elif
-
+print()
 # 2. Make the following program work. (3 mistakes)  (3pts)
      
 x = int(input("Enter a number: "))
 if x == 3:
-    print("You entered 3")
+    print("You Entered 3")
+else:
+    print("Number is Not 3")
 #Line nine should have a int()
 #2Line 18 should have a : at the end
 #3Line 18 should have == not a =
-
-  # 3. Make the following program work. (4 mistakes)  (4pts)
+print()
+# 3. Make the following program work. (4 mistakes)  (4pts)
      
 answer = input("What is the name of Poe Dameron's Droid? ")
-if answer == "BB8":
+if answer == "BB8" or answer == "bb8":
     print("Correct!")
 else:
     print("Incorrect! It is BB8.")
 #1 used diffent varibles, a is not defined
 #2Line 29 should have a : after else
 #3 Line 29 should be a == not a =
-#4
-
-  # 4. Make the following program work. (4 mistakes) (4pts)
+#4 it should except lower case bb8
+print()
+# 4. Make the following program work. (4 mistakes) (4pts)
      
 x = input("Name one of the top 3 greatest Jedi.")
-if x == "Yoda" or x == "Luke Skywalker" or x == "Obi-Wan Kenobi":
-    print ("That is correct!")
+if x == "Yoda" or x == "yoda" or x == "Luke Skywalker" or x == "luke skywalker" or x == "Obi-Wan Kenobi" or x == "obi-wan kenobi":
+    print("That is correct!")
 
-#1diffrent varibles used x is defined not jedi
+#1 diffrent varibles used x is defined not jedi
 #2 the equation needs to be printed out every if
 #3Missing () on line 39
-#4 Input anwers are not in ""
-
- # 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
- #    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.  (6pts)
+#4 Input answers are not in ""
+print()
+# 5. Make the following program work whether they enter a, A, Jedi Master or jedi master
+#    Print "Not a choice!" if they don't choose any of the three and set sensitivity to blank text.  (6pts)
 
 print("Welcome to the Jedi Academy!")
 
@@ -62,12 +64,10 @@ elif user_input == "B" or user_input == "b" or user_input == "Sith Lord" or user
 elif user_input == "C" or user_input == "c" or user_input == "Droid" or user_input == "droid":
     sensitivity = 0
 else:
-    senstivity = "Not a choice"
-print("Sensitivity: ",sensitivity)
+    sensitivity = "Not a choice"
+print("Sensitivity: ", sensitivity)
 
-
-
-
+print()
 '''
 6. NUMBER ANALYSIS PROGRAM  (10pts)
 -----------------------
@@ -90,11 +90,27 @@ Out:  Test 1: Odd
 '''
 
 number = int(input("Input any number"))
-%=2
-if number == 0
+even_odd = number
+pos_neg = number
+inc_exc = number
+even_odd%=2
+print(even_odd)
+print(pos_neg)
+print(inc_exc)
+if even_odd == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+if pos_neg > 0:
+    print("The Number is Positive")
+else:
+    print("The Number is Negative")
+if inc_exc < 100 or inc_exc > -100:
+    print("It is Inclusive")
+else:
+    print("It is Exclusive")
 
-
-
+print()
 '''
 GRADING 2.0    (10pts)
 -------------------
@@ -102,14 +118,23 @@ Copy your Grading 1.0 program below and then modify it to also print out the let
 If they fail, tell them to "Transfer to Johnston!"
 '''
 
+print("Last program is a grade calculator!X")
+sem_grade = int(input("What is your semester grade?"))
+fin_grade = int(input("What is your Final Exam grade?"))
+ex_worth = int(input("What is the Final Exam worth?"))
+sem_grade_worth = 100 - ex_worth
+sem_grade_worth /= 100
+ex_worth /= 100
+grade = (sem_grade_worth * sem_grade) + (ex_worth * fin_grade)
+print(grade)
 if grade >= 90:
-    print("A")
-elif grade>= 80:
-    print("B")
-elif grade >=70:
-    print("C")
-elif grade>=60:
-    print("D")
+    print("Final Grade = A")
+elif grade >= 80:
+    print("Final Grade = B")
+elif grade >= 70:
+    print("Final Grade = C")
+elif grade >= 60:
+    print("Final Grade = D")
 else:
-    print("F, Transfer to Johnston")
+    print("Final Grade = F, Transfer to Johnston")
 
--- Quiz_Master_Project.py
+++ Quiz_Master_Project.py
@@ -4,33 +4,68 @@ QUIZ MASTER PROJECT  (4pts)
 The criteria for the project are on the website. Make sure you test this quiz with 
 two of your student colleagues before you run it by your instructor.
 '''
+print("Welcome To My Quiz, This Quiz Is About Riddles ")
 answer_count = 0
+correct_answer = 0
+print()
 
 print("Question 1")
-answer_count+=1
+rid1 = int(input("I am a odd number, Take away a letter and I become even. What number am I?(Answer In Number Form)"))
+answer_count += 1
+if rid1 == 7:
+    correct_answer += 1
+    print("This Is Correct! Your Score Is Now: ", correct_answer, "/", answer_count)
+else:
+    print("Wrong :( The Answer Was 7, Seven - S = even, Get it?. Your Score Is Now", correct_answer, "/", answer_count)
 print()
 
 print("Question 2")
-answer_count+=1
+answer_count += 1
+print("What has to be broken before you can use it?")
+print("A. s")
+print("B. An Egg")
+print("C.")
+print("D. ")
+rid2 = input("What is the answer?")
+if rid2 == "B" or rid2 == "b":
+    correct_answer += 1
+    print("This Is Correct! Your Score Is Now: ", correct_answer, "/", answer_count)
+else:
+    print("Wrong :( The Answer Was B. An Egg. Your Score Is Now", correct_answer, "/", answer_count)
 print()
 
+
 print("Question 3")
-answer_count+=1
+rid3 = input(" David’s parents have three sons: Snap, Crackle, and what’s the name of the third son? ")
+answer_count += 1
+if rid3 == "David" or rid3 == "david":
+    correct_answer += 1
+    print("This Is Correct! Your Score Is Now: ", correct_answer, "/", answer_count)
+else:
+    print("Wrong :( The Answer Was David. Your Score Is Now", correct_answer, "/", answer_count)
 print()
 
 print("Question 4")
-answer_count+=1
+answer_count += 1
 print()
 
+
 print("Question 5")
-answer_count+=1
+answer_count += 1
+print("It belongs to you, but other people use it more than you do. What is it?")
+print("A.")
+print("B.")
+print("C.")
+print("D. Your Name ")
+
+rid5 = input()
 print()
 
 print("Question 6")
-answer_count+=1
+answer_count += 1
 print()
 
 print("Question 7")
-answer_count+=1
+answer_count += 1
 
 print(answer_count)
